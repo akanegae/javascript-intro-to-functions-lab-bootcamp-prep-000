f90701d27adfd1929794e078e729c3cc46a9fb56
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-20 08:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,16 +12,13 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  var lowercase = 'hello'
-  lowercase.toLowerCase() === lowercase // true
-  console.log('hello'.toLowerCase)
-  return `${"I can't hear you"}!`
-
-  var uppercase = "HELLO!"
-  uppercase.toUpperCase() === uppercase //true
-  console.log("HELLO!".toUpperCase)
-  return `${"YES INDEED"}!`
-
-  console.log("I love you, Grandma.")
-  return `${"I love you, too"}.`
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
